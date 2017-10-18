0f112fe38a8d6b7d6d3a0789c4663587a564e078
Lorenz <loro2008@gmx.de>
2017-10-18 12:32:47 +0200

    made new classes

---
+++ src/main/java/de/fhro/inf/prg3/a03/PlayfulState.java
@@ -0,0 +1,7 @@
+package de.fhro.inf.prg3.a03;
+
+/**
+ * Created by loro2 on 18.10.2017.
+ */
+public class PlayfulState {
+}
---
+++ src/main/java/de/fhro/inf/prg3/a03/State.java
@@ -0,0 +1,16 @@
+package de.fhro.inf.prg3.a03;
+
+import org.apache.logging.log4j.LogManager;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by loro2 on 18.10.2017.
+ */
+public class State {
+    Logger logger;
+    Animal animal;
+    int t;
+    int duration;
+
+}
